c60c20dcad301832f17022bf065788163d4154f5
upasanadash <upasanadash95@gmail.com>
2021-12-02 20:04:04 +0530

    complete commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src="https://robohash.org/fgfr" className="App-logo" alt="logo" />
+      <h1 className="App-title">Annateda</h1>
+    </header>
+    <p className="App-intro">
+      Welcome to Annateda site. This is the new release Movie <strong> The Hunting season is begin..<i> Annabella ka badla le ke aaya Annateda </i> </strong>.Must watch!
+    </p>
+    <div> created by Upasana Dash </div>
+  </div>
+  );
+}
+
+
+export default App;
